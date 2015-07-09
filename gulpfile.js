d3ef895c489922227e68163d1669575ddf873de2
tch3ung <tim@cheung.io>
2015-07-09 13:25:11 +0200

    added gulp packages

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    bourbon = require('node-bourbon').includePaths,
+    concat = require('gulp-concat');
+
+gulp.task('css', function () {
+    return gulp.src('src/styles/main.scss')
+    .pipe(sass({
+        includePaths: bourbon
+    }))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(minifyCSS())
+    .pipe(rename({
+        basename: 'readmore-flat',
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/assets/css'))
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+        'src/scripts/vendor/*js',
+        'src/scripts/*.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+});
+
+gulp.task('default', ['css', 'js'], function () {
+    gulp.watch("src/styles/**/*.scss", ['css']);
+    gulp.watch("src/scripts/**/*.js", ['js']);
+});
