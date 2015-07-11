d640d140bed50db4d2124f3fefad0a928ea3939f
Tim Cheung <tim@cheung.io>
2015-07-11 11:34:12 +0200

    moved manifest task to bottom

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,6 @@ var package = require('./package.json');
 var dist = './dist';
 var src = './src';
 
-
-gulp.task('manifest', function() {
-    return gulp.src(src + 'manifest.json')
-    .pipe(replace('{{package.title}}', package.title))
-    .pipe(replace('{{package.description}}', package.description))
-    .pipe(replace('{{package.version}}', package.version))
-    .pipe(replace('{{package.author}}', package.author))
-    .pipe(gulp.dest(dist))
-});
-
 gulp.task('css', function () {
     return gulp.src(src + '/stylesheets/main.scss')
     .pipe(plumber({
@@ -62,8 +52,17 @@ gulp.task('js', function() {
     .pipe(gulp.dest(dist + '/assets/js'))
 });
 
-gulp.task('default', ['manifest', 'css', 'js'], function () {
-    gulp.watch(src + 'manifest.json', ['manifest']);
+gulp.task('manifest', function() {
+    return gulp.src(src + 'manifest.json')
+    .pipe(replace('{{package.title}}', package.title))
+    .pipe(replace('{{package.description}}', package.description))
+    .pipe(replace('{{package.version}}', package.version))
+    .pipe(replace('{{package.author}}', package.author))
+    .pipe(gulp.dest(dist))
+});
+
+gulp.task('default', ['css', 'js', 'manifest'], function () {
     gulp.watch(src + '/stylesheets/**/*.scss', ['css']);
     gulp.watch(src + '/javascripts/**/*.js', ['js']);
+    gulp.watch(src + 'manifest.json', ['manifest']);
 });
