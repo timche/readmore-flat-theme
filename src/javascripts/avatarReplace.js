b71829a2961ffbb7b5a2de10f2e94e2cc2d1beb7
Tim Cheung <tim@cheung.io>
2015-07-12 17:23:33 +0200

    replaced default avatar

---
+++ src/javascripts/avatarReplace.js
@@ -0,0 +1,2 @@
+avatarDefault = chrome.extension.getURL("assets/img/avatar-default.png");
+$('img[src$="https://cdn0.readmore.de/user_pictures/default/default/100.jpg?0"]').attr('src', avatarDefault);
