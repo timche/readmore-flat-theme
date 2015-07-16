a9b1a480da4f5042b80c820ec62ef9cfc81f8a47
tch3ung <tim@cheung.io>
2015-07-16 13:26:24 +0200

    replaced image icons with font awesome

---
+++ src/theme/javascripts/replaceIconImages.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('img[src$="https://cdn1.readmore.de/img/themes/readmore/side_left_setup2.png"]').replaceWith('<i class="fa fa-cog"></i>');
+  $('img[src$="https://cdn1.readmore.de/img/themes/readmore/icon_plus.jpg"]').replaceWith('<i class="fa fa-plus"></i>');
+  $('img[src$="https://cdn1.readmore.de/img/themes/readmore/icon_minus.jpg"]').replaceWith('<i class="fa fa-minus"></i>');
+  $('img[src$="https://cdn1.readmore.de/img/themes/readmore/arrow_last_item.gif"]').replaceWith('<i class="fa fa-angle-right"></i>');
+  $('img[src$="https://cdn1.readmore.de/img/themes/readmore/arrow_last_item2.gif"]').replaceWith('<i class="fa fa-angle-double-right"></i>');
+});
