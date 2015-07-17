0c33859361edab1cebc74bad9ea02eaec31919d6
Tim Cheung <tim@cheung.io>
2015-07-17 08:08:06 +0200

    import fonts over js first

--- src/theme/javascripts/applyTheme.js
+++ src/theme/javascripts/applyTheme.js
@@ -1,5 +1,7 @@
 chrome.storage.sync.get('preferedThemeStyle', function (options) {
+    var stylesheetPathThemeFonts = chrome.extension.getURL('assets/css/readmore-flat-theme-fonts.css');
+    $('head').prepend('<link href=' + stylesheetPathThemeFonts + ' rel="stylesheet" type="text/css">');
     var preferedThemeStyle = options['preferedThemeStyle'];
-    var stylesheetPath = chrome.extension.getURL('assets/css/readmore-flat-theme-' + preferedThemeStyle + '.min.css');
-    $('head').append('<link href=' + stylesheetPath + ' rel="stylesheet" type="text/css">');
+    var stylesheetPathTheme = chrome.extension.getURL('assets/css/readmore-flat-theme-' + preferedThemeStyle + '.min.css');
+    $('head').append('<link href=' + stylesheetPathTheme + ' rel="stylesheet" type="text/css">');
 });
