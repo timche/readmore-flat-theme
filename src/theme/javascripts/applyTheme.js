fb41b9625c7f9ea49a4f608d406ebb15eb2a48e2
Tim Cheung <tim@cheung.io>
2015-07-18 11:00:04 +0200

    Revert "import fonts over js first"
    This reverts commit 0c33859361edab1cebc74bad9ea02eaec31919d6.

--- src/theme/javascripts/applyTheme.js
+++ src/theme/javascripts/applyTheme.js
@@ -1,7 +1,5 @@
 chrome.storage.sync.get('preferedThemeStyle', function (options) {
-    var stylesheetPathThemeFonts = chrome.extension.getURL('assets/css/readmore-flat-theme-fonts.css');
-    $('head').prepend('<link href=' + stylesheetPathThemeFonts + ' rel="stylesheet" type="text/css">');
     var preferedThemeStyle = options['preferedThemeStyle'];
-    var stylesheetPathTheme = chrome.extension.getURL('assets/css/readmore-flat-theme-' + preferedThemeStyle + '.min.css');
-    $('head').append('<link href=' + stylesheetPathTheme + ' rel="stylesheet" type="text/css">');
+    var stylesheetPath = chrome.extension.getURL('assets/css/readmore-flat-theme-' + preferedThemeStyle + '.min.css');
+    $('head').append('<link href=' + stylesheetPath + ' rel="stylesheet" type="text/css">');
 });
